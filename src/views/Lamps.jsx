122c2e816ba2675420562c45c84ed560bcaf8672
rafeira <rafael12wq@gmail.com>
2023-02-04 22:27:38 -0300

    Implement lamps styles and add lamps

--- src/views/Lamps.jsx
+++ src/views/Lamps.jsx
@@ -1,9 +1,23 @@
+import Lamp from '../components/lamps/Lamp';
 import './Lamps.css'
-
+import bulbOff from '../images/light_bulb_off.jpg'
 const Lamps = (props) => {
   return (
     <div className='Lamps'>
-      Lamps
+      <div className="lamps-container">
+        <div className='add-button'>+</div>
+
+        <Lamp src={bulbOff}/>
+        <Lamp src={bulbOff}/>
+        <Lamp src={bulbOff}/>
+        <Lamp src={bulbOff}/>
+        <Lamp src={bulbOff}/>
+        <Lamp src={bulbOff}/>
+        <Lamp src={bulbOff}/>
+        <Lamp src={bulbOff}/>
+        <Lamp src={bulbOff}/>
+
+      </div>
     </div>
   );
 }
