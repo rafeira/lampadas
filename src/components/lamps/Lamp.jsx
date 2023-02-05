26749da3033de7d413a2cb0f3f88f8f97943d035
rafeira <rafael12wq@gmail.com>
2023-02-05 20:06:21 -0300

    Create functions to change lamp src

--- src/components/lamps/Lamp.jsx
+++ src/components/lamps/Lamp.jsx
@@ -1,11 +1,29 @@
 import './Lamp.css'
 
-const Lamp = (props) => {
+import bulbOff from '../../images/light_bulb_off.jpg'
+import bulbOn from '../../images/light_bulb_on.jpg'
+import brokenBulb from '../../images/broken_light_bulb.jpg'
+import { useState } from 'react'
+
+const Lamp = () => {
+  const [src, setSrc] = useState(bulbOff)
+
+  const brokeBulb = () => {
+    setSrc(brokenBulb)
+  }
+  const turnOn = () => {
+    if (src != brokenBulb)
+      setSrc(bulbOn)
+  }
+  const turnOff = () => {
+    if (src != brokenBulb)
+      setSrc(bulbOff)
+  }
   return (
     <div className="Lamp">
-      <img src={props.src} />
+      <img src={src} onClick={brokeBulb} onMouseOut={turnOff} onMouseOver={turnOn} />
     </div>
   );
-} 
+}
 
 export default Lamp;
--- src/views/Lamps.jsx
+++ src/views/Lamps.jsx
@@ -1,20 +1,16 @@
+import { useState } from 'react';
 import Lamp from '../components/lamps/Lamp';
 import './Lamps.css'
-import bulbOff from '../images/light_bulb_off.jpg'
-import { useEffect, useState } from 'react';
 const Lamps = (props) => {
   const [lamps, setLamps] = useState([])
 
-  function onAddButtonPressed() {
-    console.log('entrou')
-    const oldarray = lamps; 
-    setLamps(() => [...oldarray, bulbOff])
-  }
+  const onAddButtonPressed = () => setLamps(() => [...lamps, <Lamp/>])
+
   return (
     <div className='Lamps'>
       <div className="lamps-container">
         <div className='add-button' onClick={onAddButtonPressed}>+</div>
-        {lamps.map((e) => <Lamp src={e}></Lamp>)}
+        {lamps}
       </div>
     </div>
   );
