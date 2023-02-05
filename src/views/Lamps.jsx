52042e45266eff49cbaa1401e631c67f61fa3809
rafeira <rafael12wq@gmail.com>
2023-02-04 23:03:19 -0300

    Enable to add lamps according to user desire

--- src/views/Lamps.jsx
+++ src/views/Lamps.jsx
@@ -1,25 +1,24 @@
 import Lamp from '../components/lamps/Lamp';
 import './Lamps.css'
 import bulbOff from '../images/light_bulb_off.jpg'
+import { useEffect, useState } from 'react';
 const Lamps = (props) => {
+  const [lamps, setLamps] = useState([])
+
+  function onAddButtonPressed() {
+    console.log('entrou')
+    const oldarray = lamps; 
+    setLamps(() => [...oldarray, bulbOff])
+  }
   return (
     <div className='Lamps'>
       <div className="lamps-container">
-        <div className='add-button'>+</div>
-
-        <Lamp src={bulbOff}/>
-        <Lamp src={bulbOff}/>
-        <Lamp src={bulbOff}/>
-        <Lamp src={bulbOff}/>
-        <Lamp src={bulbOff}/>
-        <Lamp src={bulbOff}/>
-        <Lamp src={bulbOff}/>
-        <Lamp src={bulbOff}/>
-        <Lamp src={bulbOff}/>
-
+        <div className='add-button' onClick={onAddButtonPressed}>+</div>
+        {lamps.map((e) => <Lamp src={e}></Lamp>)}
       </div>
     </div>
   );
+
 }
 
 export default Lamps;
